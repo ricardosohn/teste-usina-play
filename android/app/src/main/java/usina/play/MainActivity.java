b27f05b764f22d742b304716fde24bd6e7c92042
Ricardo Sohn <ricardosohn@gmail.com>
2023-03-03 21:34:26 -0300

    chore: initial commit

---
+++ android/app/src/main/java/usina/play/MainActivity.java
@@ -0,0 +1,5 @@
+package usina.play;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'usina.play',
+  appName: 'usina-play',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
