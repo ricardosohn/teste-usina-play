07537b7e58f1ee0092c296ef372e4a0beb4e26a9
Ricardo Sohn <ricardosohn@gmail.com>
2023-03-06 14:47:54 -0300

    fix(app-header): adicionado parametro para remover a borda da toolbar condicionalmente

--- src/app/features/manage-schedule/presentation/page/components/app-header/app-header.component.ts
+++ src/app/features/manage-schedule/presentation/page/components/app-header/app-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-header.component.scss'],
 })
 export class AppHeaderComponent  implements OnInit {
-
-  constructor() { }
-
+  @Input() hotfix: boolean = false;
+  
   ngOnInit() {}
 
 }
